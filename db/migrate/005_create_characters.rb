8d158735ffc122179f434c5203d5c98d2ab19c2c
Abdul Khan <akhan17@outlook.com>
2020-05-19 17:35:15 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration
   def change
     create_table :characters do |t|
+        t.text
+        t.text
+        t.belongs_to
+        t.belongs_to
     end
   end
 end
